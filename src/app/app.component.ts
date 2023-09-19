f776e59dc569e65e6b7414e9ef78cddfacbd482f
blackspy03 <albertchrishansenxd@gmail.com>
2023-09-19 20:11:19 +0700

    Kelar Minggu 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'A_AngularBootstrap';
+  title = 'Angular Bootstrap';
+  nama = 'Albert Chrishansen';
+  nim = '212102386';
 }
